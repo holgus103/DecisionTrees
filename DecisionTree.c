7ea8113dab3e4b1a2c6794f52689bdecf788554f
holgus103 <holgus103@gmail.com>
2017-06-03 16:08:11 +0200

    Something is happening but is it really working?

--- DecisionTree.c
+++ DecisionTree.c
@@ -2,7 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+//#define DEBUG
 
+
+#ifdef DEBUG
+void DisplayArray(double** elements, int count, int attributes){
+	int i, j;
+	// foreach element
+	for(i = 0; i < count; i++){
+		//foreach attribute 
+		for(j = 0; j < attributes + 1; j++){
+			if(elements[i] != NULL){
+				printf("%f ", elements[i][j]);
+			}
+		} 
+		printf("\n");
+	} 
+}
+#endif
 double CalculateGiniForSet(double** elements, int count, int classes){
 	int i, j;
 	double result = 0;
@@ -43,6 +60,9 @@ void TrySplit(int index, int value, double** elements, int elementsCount, double
 }
 
 void Split(TreeNode* node, double** elements, int elementsCount, int attributesCount, int classCount){
+	#ifdef DEBUG
+	//DisplayArray(elements, elementsCount, attributesCount);
+	#endif
 	double** left, ** right;
 	int bestIndex, 	i, j, k, leftCount, rightCount;
 	double leftGini, rightGini, bestLeftGini, bestRightGini;
@@ -71,17 +91,23 @@ void Split(TreeNode* node, double** elements, int elementsCount, int attributesC
 				}
 			}
 	}
+	#ifdef DEBUG
+	printf("Found best split with attribute: %d, value: %f, Gini: %f\n", bestIndex, bestSplitValue, bestGini); 
+	#endif
 	// split using best params
 	rightCount = leftCount = 0;
 	memset(left, 0, sizeof(double*)*elementsCount);	
 	TrySplit(bestIndex, bestSplitValue, elements, elementsCount, left, right, &leftCount, &rightCount);
+	free(elements);
 	node->attributeValue = bestSplitValue;
 	node->attributeIndex = bestIndex;
-	node->data = elements;
 	node->left = malloc(sizeof(TreeNode));
 	node->right = malloc(sizeof(TreeNode));
 	// left node
-	if(bestLeftGini > 0){
+	if(bestLeftGini > 0 && bestLeftGini< 1){
+		#ifdef DEBUG
+		printf("splitting left with gini: %f\n", bestLeftGini);
+		#endif
 		Split(node->left, left, leftCount, attributesCount, classCount); 
 	}
 	else{
@@ -89,7 +115,10 @@ void Split(TreeNode* node, double** elements, int elementsCount, int attributesC
 		node->left->data = left;
 	}
 	// right node
-	if(bestRightGini > 0){
+	if(bestRightGini > 0 && bestRightGini< 1){
+		#ifdef DEBUG
+		printf("splitting right with gini: %f\n", bestRightGini);
+		#endif
 		Split(node->right, right, leftCount, attributesCount, classCount); 
 	}
 	else{
@@ -102,7 +131,9 @@ void DisposeTree(TreeNode* node){
 	if(node == NULL) return;
 	DisposeTree(node->left);
 	DisposeTree(node->right);
-	free(node->data);
+	if(node->data){
+		free(node->data);
+	}
 	free(node);
 }
 
--- DecisionTree.h
+++ DecisionTree.h
@@ -1,3 +1,4 @@
+#define DEBUG
 #ifndef DecisionTrees_H
 typedef struct a {
 	double** data;
@@ -7,6 +8,9 @@ typedef struct a {
 	struct a* left;
 	struct a* right;
 } TreeNode;
+#ifdef DEBUG
+void DisplayArray(double** elements, int count, int attributesCount);
+#endif
 double CalculateGiniForSet(double** elements, int count, int classes);
 void TrySplit(int index, int value, double** elements, int elementsCount, double** left, double** right, int* leftCount, int* rightCount);
 void Split(TreeNode* node, double** elements, int elementsCount, int attributesCount, int classCount);
